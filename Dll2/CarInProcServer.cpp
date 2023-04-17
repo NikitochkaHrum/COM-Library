a6cd68d6f1fa54f7fdbc6f1f41c7d4a01f0d1aa6
NikitochkaHrum <ponkin.nikitka@mail.ru>
2023-04-17 08:35:18 +0500

    Добавьте файлы проекта.

---
+++ Dll2/CarInProcServer.cpp
@@ -0,0 +1,33 @@
+﻿#include <windows.h>
+#include "CoCarClassFactory.h"
+
+ULONG g_lockCount = 0;
+ULONG g_objCount = 0;
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ppv)
+{
+	HRESULT hr;
+	CoCarClassFactory* pCFact = NULL;
+
+	if (rclsid != CLSID_CoCar)
+		return CLASS_E_CLASSNOTAVAILABLE;
+
+	pCFact = new CoCarClassFactory;
+
+	hr = pCFact->QueryInterface(riid, ppv);
+
+	if (FAILED(hr))
+		delete pCFact;
+
+	return hr;
+}
+
+STDAPI DllCanUnloadNow()
+{
+	if (g_lockCount == 0 && g_objCount == 0)
+	{
+		return S_OK;
+	}
+	else
+		return S_FALSE;
+}
